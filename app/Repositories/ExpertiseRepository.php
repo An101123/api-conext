48f2ec174860afa89459da3801bd1313f942ced8
TranThiAn <thian200298@gmail.com>
2021-01-26 11:04:15 +0700

    CR expertise

--- app/Repositories/ExpertiseRepository.php
+++ app/Repositories/ExpertiseRepository.php
@@ -2,14 +2,26 @@
 
 namespace App\Repositories;
 
+use App\Models\Expertise;
+
 class ExpertiseRepository implements ExpertiseRepositoryInterface{
     public function getExpertises()
     {
-        
+        return Expertise::all();
     }
 
     public function store($input)
     {
+        var_dump($input);
+        try{
+            $data = array(
+                'name' => $input['name']
+            );
+            Expertise::create($data);
+            return true;
+        } catch(\Exception $e){
+            return false;
+        }
         
     }
 
--- app/Services/ExpertiseService.php
+++ app/Services/ExpertiseService.php
@@ -2,18 +2,26 @@
 
 namespace App\Services;
 
+use App\Repositories\ExpertiseRepositoryInterface;
+
 class ExpertiseService implements ExpertiseServiceInterface{
+
+    protected $expertiseRepository;
+    
+    public function __construct(ExpertiseRepositoryInterface $expertiseRepository)
+    {
+        return $this->expertiseRepository = $expertiseRepository;
+    }
     public function getExpertises()
     {
-
+        return $this->expertiseRepository->getExpertises();
     }
     public function store($input)
     {
-
+        return $this->expertiseRepository->store($input);
     }
     public function getExpertise($id)
     {
-
     }
     public function update($input, $id)
     {
