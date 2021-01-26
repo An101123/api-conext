5950cf282f3cb11f8c95f6fb1aa353d9a30cd822
TranThiAn <thian200298@gmail.com>
2021-01-26 09:23:25 +0700

    CRUD BusinessType

--- database/migrations/2021_01_25_085457_create_users_table.php
+++ database/migrations/2021_01_25_085457_create_users_table.php
@@ -14,9 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('adress');
+            $table->integer('gender');
+            $table->integer('localConext_id')->unsigned();
+            $table->foreign('localConext_id')->references('id')->on('local_conexts')->onDelete('cascade');
+            $table->integer('businessType_id')->unsigned();
+            $table->foreign('businessType_id')->references('id')->on('business_types')->onDelete('cascade');
+            $table->integer('expertise_id')->unsigned();
+            $table->foreign('expertise_id')->references('id')->on('expertises')->onDelete('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
